c5de7f6223af737029b33f1685ae4e11c88a616d
bigdra50 <ryu1998tea@gmail.com>
2019-11-06 08:26:24 +0900

    Move function from Monobehaviour to Editor Extension

---
+++ Assets/Editor/ImportFileEditorEx.cs
@@ -0,0 +1,43 @@
+﻿using ImportFileFromGitHub;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    public class ImportFileEditorEx : EditorWindow
+    {
+        
+        [MenuItem("Window/ImportFile")]
+        static void Open()
+        {
+            EditorWindow.GetWindow<ImportFileEditorEx>("ImportFile");
+        }
+
+
+        private string _url = "";
+
+        private void OnGUI()
+        {
+            GUILayout.Space(10);
+            EditorGUILayout.LabelField("インポートしたいファイルのURLを入力して下さい.");
+            GUILayout.Label("URL");
+            _url = GUILayout.TextField(_url);
+            
+            GUILayout.Space(10);
+            
+
+            if (GUILayout.Button("Import"))
+            {
+                Debug.Log("Start Import");
+                var import = new ImportFileFromWeb();
+                import.StartImportAsync(_url);
+            }
+
+            if (GUILayout.RepeatButton("Repeat"))
+            {
+                Debug.Log("Repeat");
+            }
+
+        }
+    }
+}
