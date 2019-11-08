f056b7bd9bf83b11cefe28f0ced11905a297118e
bigdra50 <ryu1998tea@gmail.com>
2019-11-09 02:01:38 +0900

    nyannnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn

--- Assets/GLSL2ShaderLab/Scripts/Converter.cs
+++ Assets/GLSL2ShaderLab/Scripts/Converter.cs
@@ -3,17 +3,79 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using UnityEditor;
 using UnityEngine;
 
+/// <summary>
+/// glslのファイルをshaderlabのファイルへ変換する
+/// </summary>
 public class Converter
 {
-    private string _glslFilePath = Path.Combine(Application.dataPath, "GLSL2ShaderLab", "Shader", "PietMondrianPaint.glsl");
+    private readonly string _glslFilePath = Path.Combine(Application.dataPath, "GLSL2ShaderLab", "Shader", "PietMondrianPaint.glsl");
+    private readonly string _shaderPath = Path.Combine(Application.dataPath, "GLSL2ShaderLab", "Shader");
+    private string _shaderName = string.Empty;
     
+    private string _codeInsertRage = @"@block\s*(?<Block>[^\s\n]+)\s*\n(?<Value>[\s\S]*?)\n\s*(:?//\s*)*?@endblock";
+    // nyaaaaan
+    List<string> _shaderLabTemplate = new List<string>()
+    {
+        @"Shader ""Custom/Template""
+        {
+        Properties
+        {
+            _iChannel0(""iChannel0"", 2D) = ""white""{} 
+            _iChannel1(""iChannel1"", 2D) = ""white""{} 
+            _iChannel2(""iChannel2"", 2D) = ""white""{} 
+        }
+        SubShader
+        {
+        Tags { ""RenderType""=""Opaque"" }
+        LOD 100
+        
+        Pass
+        {
+            CGPROGRAM
+        #pragma vertex vert_img
+        #pragma fragment frag
+                    
+            sampler2D _iChannel0;
+            float4 _iChannel0_ST;
+            sampler2D _iChannel1;
+            float4 _iChannel1_ST;
+            sampler2D _iChannel2;
+            float4 _iChannel2_ST;
+            float4 fragColor;
+        
+        #include ""UnityCG.cginc""
+                    
+            float mod(float x, float y)
+            {
+                return x-y*floor(x/y);
+            }"
+        
+    ,@"                
+                return fragColor;
+            }
+            ENDCG
+        }
+    }
+}",
+    };
+
+
     Dictionary<string, string> _glslHlslDic = new Dictionary<string, string>()
     {
+        {"void mainImage( out vec4 fragColor, in vec2 fragCoord )", "fixed4 frag(v2f_img i) : SV_Target"},
+        {"void mainImage(out vec4 fragColor, in vec2 fragCoord )", "fixed4 frag(v2f_img i) : SV_Target"},
+        {"void mainImage( out vec4 fragColor,in vec2 fragCoord )", "fixed4 frag(v2f_img i) : SV_Target"},
+        {"void mainImage(out vec4 fragColor,in vec2 fragCoord )", "fixed4 frag(v2f_img i) : SV_Target"},
+        {"void mainImage( out vec4 fragColor, in vec2 fragCoord)", "fixed4 frag(v2f_img i) : SV_Target"},
+        {"void mainImage(out vec4 fragColor, in vec2 fragCoord)", "fixed4 frag(v2f_img i) : SV_Target"},
+        {"void mainImage( out vec4 fragColor,in vec2 fragCoord)", "fixed4 frag(v2f_img i) : SV_Target"},
+        {"void mainImage(out vec4 fragColor,in vec2 fragCoord)", "fixed4 frag(v2f_img i) : SV_Target"},
         {"vec2", "float2"},
-        {"vec3", "vec3"},
-        {"vec4", "vec4"},
+        {"vec3", "float3"},
+        {"vec4", "float4"},
         {"mat3", "float3x3"},
         {"mat4", "float4x4"},
         {"mat3x4", "float3x4"},
@@ -43,21 +105,44 @@ public class Converter
         {"fract(", "frac("},
         {"atan(", "atan2("},
         {"iTime", "_Time.y"},
+        {"iMouse.xy", "float2(1.0, 1.0)"},    // TODO: マウス入力
+        {"iMouse.x", "1"},    
+        {"iMouse.y", "1"},   
         {"iResolution", "_ScreenParams"},
         {"fragCoord", "i.uv*_ScreenParams"},
         {"iChannel0", "_iChannel0"},
         {"iChannel1", "_iChannel1"},
         {"iChannel2", "_iChannel2"},
     };
-    
+
+    public Converter()
+    {
+        
+    }
+
+    public Converter(string fileName)
+    {
+        this._shaderName = fileName;
+    }
     public void Start()
     {
-        var hlslLine = File.ReadLines(_glslFilePath, Encoding.UTF8)
+        var hlslLines = File.ReadLines(_glslFilePath, Encoding.UTF8)
             .Select(s => s = ReplaceGlsl2Hlsl(s));
-        
-        foreach (var l in hlslLine)
+
+        using (var writer = new StreamWriter(Path.Combine(_shaderPath, _shaderName)))
         {
-            Debug.Log(l);
+            writer.WriteLine(_shaderLabTemplate.First());
+            var cnt = 0;
+            foreach (var hlslLine in hlslLines)
+            {
+                if (cnt < 44)
+                {
+                    writer.WriteLine(hlslLine);
+                }
+                cnt++;
+            }
+            writer.WriteLine(_shaderLabTemplate.Last());
+            AssetDatabase.ImportAsset(FileUtil.GetProjectRelativePath(Path.Combine(_shaderPath, _shaderName)));
         }
         
         // if (File.Exists(_glslFilePath))
@@ -75,9 +160,15 @@ public class Converter
 
     public string ReplaceGlsl2Hlsl(string glslLine)
     {
-        var hlslLine = string.Empty;
+        var hlslLine = glslLine;
         // TODO: とりあえずdicから置き換えるだけのを実装
-        
+        foreach (var dic in _glslHlslDic)
+        {
+            if (hlslLine.Contains(dic.Key))
+            {
+                hlslLine = hlslLine.Replace(dic.Key, dic.Value);
+            }
+        }
         
         
         return hlslLine;
--- Assets/GLSL2ShaderLab/Tests/EditorMode/Editor/GLSL2HLSLReplaceTest.cs
+++ Assets/GLSL2ShaderLab/Tests/EditorMode/Editor/GLSL2HLSLReplaceTest.cs
@@ -14,6 +14,9 @@ namespace Tests
             "vec3 rect(vec2 bl, vec2 tr, vec2 st){",
             "float d = .02;",
             "vec3 r2 = vec3(blInv.x * trInv.x * blInv.y * trInv.y);",
+            "	vec2 uv = (seed=32.+seed*fract(seed))+vec2(78.233, 10.873);",
+            "            rotmat = mat3(cs, -sn, 0.0,",
+            "    vec2 mo = iMouse.xy/iResolution.xy;"
         };
         
         [Test]
@@ -45,6 +48,29 @@ namespace Tests
             var converter = new Converter();
             var line = converter.ReplaceGlsl2Hlsl(_testLine[2]);
             Assert.AreEqual("float3 r2 = float3(blInv.x * trInv.x * blInv.y * trInv.y);", line);
+        }
+        
+        [Test]
+        public void ReplaceGlsl2HlslLineTest4()
+        {
+            var converter = new Converter();
+            var line = converter.ReplaceGlsl2Hlsl(_testLine[3]);
+            Assert.AreEqual("	float2 uv = (seed=32.+seed*frac(seed))+float2(78.233, 10.873);", line);
+        }
+        [Test]
+        public void ReplaceGlsl2HlslLineTest5()
+        {
+            var converter = new Converter();
+            var line = converter.ReplaceGlsl2Hlsl(_testLine[4]);
+            Assert.AreEqual("            rotmat = float3x3(cs, -sn, 0.0,", line);
+        }
+        
+        [Test]
+        public void ReplaceGlsl2HlslLineTest6()
+        {
+            var converter = new Converter();
+            var line = converter.ReplaceGlsl2Hlsl(_testLine[5]);
+            Assert.AreEqual("    float2 mo = float2(1.0, 1.0)/_ScreenParams.xy;", line);
         }
     }
 }
