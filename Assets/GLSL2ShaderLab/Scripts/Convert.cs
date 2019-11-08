810057692a5258d960823fbaea18ceea962d4b55
bigdra50 <ryu1998tea@gmail.com>
2019-11-08 11:47:01 +0900

    add glgl2hlsl Dictionary

--- Assets/GLSL2ShaderLab/Scripts/Convert.cs
+++ Assets/GLSL2ShaderLab/Scripts/Convert.cs
@@ -21,4 +21,45 @@ public class Converter
             }
         }
     }
+    
+    Dictionary<string, string> _glslHlslDic = new Dictionary<string, string>()
+    {
+        {"vec2", "float2"},
+        {"vec3", "vec3"},
+        {"vec4", "vec4"},
+        {"mat3", "float3x3"},
+        {"mat4", "float4x4"},
+        {"mat3x4", "float3x4"},
+        {"ivec2", "int2"},
+        {"ivec3", "int3"},
+        {"ivec4", "int4"},
+        {"uvec2", "uint2"},
+        {"uvec3", "uint3"},
+        {"uvec4", "uint4"},
+        {"bvec2", "bool2"},
+        {"bvec3", "bool3"},
+        {"bvec4", "bool4"},
+        {"lowp float", "min10float"},
+        {"midiump float", "half(min16float)"},
+        {"highp float", "float"},
+        {"highp int", "int"},
+        {"highp uint", "uint"},
+        {"midiump uint", "min16uint"},
+        {"midiump int", "min16int"},
+        {"lowp int", "min12int"},
+        {"lowp uint", "min12int"},
+        {"inversesqrt(", "rsqrt("},
+        {"mix(", "lerp("},
+        {"mod(", "fmod("},    // ちょっと違うので注意
+        {"texture(", "tex2D("},
+        {"texture2D(", "tex2D("},
+        {"fract(", "frac("},
+        {"atan(", "atan2("},
+        {"iTime", "_Time.y"},
+        {"iResolution", "_ScreenParams"},
+        {"fragCoord", "i.uv*_ScreenParams"},
+        {"iChannel0", "_iChannel0"},
+        {"iChannel1", "_iChannel1"},
+        {"iChannel2", "_iChannel2"},
+    };
 }
